3b13d442a79fae686625bc9b406312852b4f24c5
Carter Sloan <cartersloan29@gmail.com>
2019-03-07 17:33:46 -0700

    added idea item and modified view board page. still incomplete

---
+++ src/components/IdeaItem.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Typography from '@material-ui/core/Typography';
+import Chip from '@material-ui/core/Chip';
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+
+const styles = theme => {
+  console.log("Text", theme);
+  return {
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '33.33%',
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+  icon: {
+    verticalAlign: 'bottom',
+    height: 20,
+    width: 20,
+  },
+  details: {
+    alignItems: 'center',
+  },
+  column: {
+    flexBasis: '33.33%',
+  },
+  helper: {
+    borderLeft: `2px solid ${theme.palette.divider}`,
+    padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`,
+  },
+  link: {
+    color: theme.palette.primary.main,
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  },
+  }};
+
+class IdeaItem extends Component {
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+      <ExpansionPanel>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography className={classes.heading}>Title here</Typography>
+          <Typography className={classes.secondaryHeading}>number of votes here</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <div className={classes.column}>
+            <Chip label="Barbados" className={classes.chip} onDelete={() => {}} />
+          </div>
+          <div className={classNames(classes.column, classes.helper)}>
+            <Typography variant="caption">
+              Select your destination of choice
+              <br />
+              <a href="#sub-labels-and-columns" className={classes.link}>
+                Learn more
+              </a>
+            </Typography>
+          </div>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </div>
+    )
+  }
+}
+
+IdeaItem.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(IdeaItem);
+
--- src/components/ViewBoardPage.js
+++ src/components/ViewBoardPage.js
@@ -1,13 +1,57 @@
 import React, { Component } from 'react';
+import IdeaItem from './IdeaItem';
+import { withStyles } from '@material-ui/core/styles';
+import { createReadStream } from 'fs';
+import { connect } from 'react-redux';
+import Typography from '@material-ui/core/Typography';
 
+const styles = {
+  ideaColumn: {
+    minWidth: 275,
+    maxWidth: 650,
+    margin: 'auto',
+    width: '75%',
+  },
+  ideaList: {
+
+  },
+  boardTitle: {
+    margin: "20px 0 20px 0",
+    fontSize: "30px"
+  }
+};
+
+//TODO: make expansion panels collapse when a different panel is clicked
+//this is outlined in the second example of material ui react page for expansion panels
 class ViewBoardPage extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        Test view
+      <div className={classes.ideaColumn}>
+        <Typography className={classes.boardTitle}>
+          Board title here
+        </Typography>
+        <div>
+          <Typography>
+            Num of Ideas here
+          </Typography>
+          <Typography>
+            Num of your votes left
+          </Typography>
+        </div>
+        <div className={classes.ideaList}>
+            <IdeaItem></IdeaItem>
+        </div>
       </div>
     )
   }
 }
 
-export default ViewBoardPage;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    currentBoard: state.currentBoard,
+  }
+}
+
+export default withStyles(styles)(connect(mapStateToProps, null)(ViewBoardPage));
+
